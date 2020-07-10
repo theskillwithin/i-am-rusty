74a7508b44d57f27880af8393f7c7bad3614a05e
theskillwithin <austinppeterson@gmail.com>
2020-07-09 19:55:57 -0600

    array map

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    let a_iter = a.iter();
+
+    let b: Vec<i32> = a_iter.map(|x: &i32| x * 2).collect();
+
+    println!("my array: {:?}", b)
 }
